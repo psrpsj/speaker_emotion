d064613aa3a0bd165fba62236de4c725dca0b485
Sangryu Park <psrpsj@gmail.com>
2022-11-08 14:43:36 +0000

    finish inference kfold

--- inference.py
+++ inference.py
@@ -27,32 +27,66 @@ def inference():
     test_dataset = CustomDataset(dataset, tokenizer)
     dataloader = DataLoader(test_dataset, batch_size=16, shuffle=False)
 
-    model_path = os.path.join("./output/", model_args.project_name)
-    model = AutoModelForSequenceClassification.from_pretrained(model_path)
-    model.to(device)
-    model.eval()
+    if model_args.k_fold:
+        pred_prob = []
+        for fold_num in range(1, 6):
+            print(f"--- START INFERENCE FOLD {fold_num} ---")
+            model_path = os.path.join(
+                "./output/", model_args.project_name + "_kfold", "fold" + str(fold_num)
+            )
+            model = AutoModelForSequenceClassification.from_pretrained(model_path)
+            model.to(device)
+            model.eval()
 
-    output_prob = []
-    output_pred = []
+            output_prob = []
 
-    print("### START INFERENCE ###")
-    for data in tqdm(dataloader):
-        output = model(
-            input_ids=data["input_ids"].to(device),
-            attention_mask=data["attention_mask"].to(device),
-            token_type_ids=data["token_type_ids"].to(device),
-        )
-        logit = output[0]
-        prob = F.softmax(logit, dim=-1).detach().cpu().numpy()
-        logit = logit.detach().cpu().numpy()
-        result = np.argmax(logit, axis=-1)
-        output_pred.append(result)
-        output_prob.append(prob)
+            for data in tqdm(dataloader):
+                with torch.no_grad():
+                    outputs = model(
+                        input_ids=data["input_ids"].to(device),
+                        attention_mask=data["attention_mask"].to(device),
+                        token_type_ids=data["token_type_ids"].to(device),
+                    )
+                    logit = outputs[0]
+                    prob = F.softmax(logit, dim=-1).detach().cpu().numpy()
+                    output_prob.append(prob)
+            output_prob = np.concatenate(output_prob, axis=0).tolist()
+            pred_prob.append(output_prob)
+            print(f"--- FINISH INFERENCE FOLD {fold_num} ---")
 
-    pred_answer = np.concatenate(output_pred).tolist()
-    output_prob = np.concatenate(output_prob, axis=0).tolist()
-    dataset["Target"] = pred_answer
-    dataset["Target"] = num_to_label(dataset["Target"])
+        pred_prob = np.sum(pred_prob, axis=0) / 5
+        pred_answer = np.argmax(pred_prob, axis=-1)
+        dataset["Target"] = pred_answer
+        dataset["Target"] = num_to_label(dataset["Target"])
+
+    else:
+        model_path = os.path.join("./output/", model_args.project_name)
+        model = AutoModelForSequenceClassification.from_pretrained(model_path)
+        model.to(device)
+        model.eval()
+
+        output_prob = []
+        output_pred = []
+
+        print("### START INFERENCE with Non-KFold ###")
+        for data in tqdm(dataloader):
+            output = model(
+                input_ids=data["input_ids"].to(device),
+                attention_mask=data["attention_mask"].to(device),
+                token_type_ids=data["token_type_ids"].to(device),
+            )
+            logit = output[0]
+            prob = F.softmax(logit, dim=-1).detach().cpu().numpy()
+            logit = logit.detach().cpu().numpy()
+            result = np.argmax(logit, axis=-1)
+            output_pred.append(result)
+            output_prob.append(prob)
+
+        pred_answer = np.concatenate(output_pred).tolist()
+        output_prob = np.concatenate(output_prob, axis=0).tolist()
+        dataset["Target"] = pred_answer
+        dataset["Target"] = num_to_label(dataset["Target"])
+
     submission = pd.DataFrame({"ID": dataset["ID"], "Target": dataset["Target"]})
     submission.to_csv(os.path.join(model_path, "submission.csv"), index=False)
     print("### INFERENCE FINISH ###")
